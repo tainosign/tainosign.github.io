fef9c283963a13b3fbe5e5b18f73bd0406faa0fe
tainosign <83518282+tainosign@users.noreply.github.com>
2025-11-05 08:18:29 +0900

    Create firebase-utils.js

---
+++ public/js/firebase-utils.js
@@ -0,0 +1,43 @@
+// firebase-config.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-app.js";
+import { getAuth, signInAnonymously, signInWithCustomToken, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-firestore.js";
+
+// === Firebase 設定 ===
+const firebaseConfig = {
+  apiKey: "AIzaSyAgLH9FWBCJy-X11vu0r3YS-VZC-B9M2xA",
+  authDomain: "setapanmarketcounter.firebaseapp.com",
+  databaseURL: "https://setapanmarketcounter-default-rtdb.firebaseio.com",
+  projectId: "setapanmarketcounter",
+  storageBucket: "setapanmarketcounter.firebasestorage.app",
+  messagingSenderId: "546423839721",
+  appId: "1:546423839721:web:70d5c12129fe6cc1594978",
+  measurementId: "G-70KHJ0P1P1"
+};
+
+// === Firebase 初期化 & 認証 ===
+export async function initializeFirebase(customToken = null) {
+  const app = initializeApp(firebaseConfig);
+  const auth = getAuth(app);
+  const db = getFirestore(app);
+
+  await new Promise((resolve, reject) => {
+    const unsubscribe = onAuthStateChanged(auth, async (user) => {
+      unsubscribe();
+      try {
+        if (!user) {
+          if (customToken) {
+            await signInWithCustomToken(auth, customToken);
+          } else {
+            await signInAnonymously(auth);
+          }
+        }
+        resolve();
+      } catch (e) {
+        reject(e);
+      }
+    }, reject);
+  });
+
+  return { db, auth };
+}
