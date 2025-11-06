f4f0849e282161d603bb1e6bdbf2afca210d6caf
tainosign <83518282+tainosign@users.noreply.github.com>
2025-11-06 23:30:07 +0900

    Create tailwind.config.js

---
+++ vue-test/tailwind.config.js
@@ -0,0 +1,15 @@
+// tailwind-config.js
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        'green-in': '#00a040',
+        'red-out': '#e53935',
+        'yellow-report': '#ffc107',
+        'cyan-view': '#17a2b8',
+        'orange-calc': '#ff9800',
+        'blue-time':'#00aff0',        
+      }
+    }
+  }
+};
