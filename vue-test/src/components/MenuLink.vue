6563f79b257195c696b87a46885ad1bcd9ed0bf3
tainosign <83518282+tainosign@users.noreply.github.com>
2025-11-06 23:24:14 +0900

    Create MenuLink.vue

---
+++ vue-test/src/components/MenuLink.vue
@@ -0,0 +1,21 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'MenuLink',
+  props: {
+    href: { type: String, required: true },
+    label: { type: String, required: true },
+    colorClass: { type: String, required: true },
+  },
+  setup(props) {
+    return { props };
+  },
+});
+</script>
+
+<template>
+  <a :href="props.href" :class="['menu-link', props.colorClass]">
+    {{ props.label }}
+  </a>
+</template>
