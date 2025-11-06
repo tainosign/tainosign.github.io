3c979cf66060646655b067f52a10db13303d4de2
tainosign <83518282+tainosign@users.noreply.github.com>
2025-11-06 16:19:24 +0900

    Update tailwind.config.js

--- react-test/tailwind.config.js
+++ react-test/tailwind.config.js
@@ -4,8 +4,18 @@ export default {
     "./index.html",
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
-  theme: {
-    extend: {},
+    theme: {
+    extend: {
+      colors: {
+        'green-in': '#00a040',
+        'red-out': '#e53935',
+        'yellow-report': '#ffc107',
+        'cyan-view': '#17a2b8',
+        'orange-calc': '#ff9800',
+        'blue-time':'#00aff0',        
+      }
+    }
   },
   plugins: [],
 }
+
