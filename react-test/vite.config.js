e90319320360c337450cdaf57ee053a70be260e7
tainosign <83518282+tainosign@users.noreply.github.com>
2025-11-06 19:19:12 +0900

    Update vite.config.js

--- react-test/vite.config.js
+++ react-test/vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { resolve } from 'path';
 
 export default defineConfig({
   plugins: [react()],
@@ -8,13 +7,7 @@ export default defineConfig({
     include: [
       'firebase/app',
       'firebase/auth',
-      'firebase/firestore' // ← ここを追加
-    ],
-  },
-  build: {
-    rollupOptions: {
-      input: resolve(__dirname, 'index.html'),
-      external: [], // Firebase を外部化しない
-    },
-  },
+      'firebase/firestore'
+    ]
+  }
 });
