2e245339efa4ef022c5dd875f4396b6d06a53899
tainosign <83518282+tainosign@users.noreply.github.com>
2025-11-06 15:53:33 +0900

    Update tailwind.config.js

--- react-test/tailwind.config.js
+++ react-test/tailwind.config.js
@@ -1,6 +1,11 @@
+/** @type {import('tailwindcss').Config} */
 export default {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
   },
+  plugins: [],
 }
