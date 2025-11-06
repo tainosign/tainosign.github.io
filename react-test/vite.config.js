eae89ece348b9f9044a821b8ace597c5381c235f
tainosign <83518282+tainosign@users.noreply.github.com>
2025-11-06 14:18:10 +0900

    Update vite.config.js

--- react-test/vite.config.js
+++ react-test/vite.config.js
@@ -1,11 +1,14 @@
-// vite.config.js
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  root: "./react-test", // ← react-testフォルダをルートに
+  root: '.', // ← ここを明示
   build: {
-    outDir: "../dist", // 出力先をルート上に
+    outDir: 'dist',
+    rollupOptions: {
+      input: resolve(__dirname, 'index.html'), // ← index.html の位置を指定
+    },
   },
 });
