4334184493de0fb49f7c7160228f1d2127dea39b
tainosign <83518282+tainosign@users.noreply.github.com>
2025-11-07 17:11:11 +0900

    Update MainMenu.vue

--- vue-test/src/components/MainMenu.vue
+++ vue-test/src/components/MainMenu.vue
@@ -1,24 +1,26 @@
 <template>
   <div class="main-content flex flex-col justify-start items-center p-4">
     <div class="w-full max-w-xl flex flex-col gap-4 py-6">
-      <a
-        href="/CounterLocal"
+      <button
+        @click="$emit('selectView', 'local')"
         class="menu-link bg-orange-calc hover:bg-orange-500 text-white p-8 rounded-2xl text-2xl font-bold flex justify-center items-center h-24"
       >
         🟠 地域優先入場カウンター
-      </a>
-      <a
-        href="/CounterEntrance"
+      </button>
+
+      <button
+        @click="$emit('selectView', 'entrance')"
         class="menu-link bg-green-in hover:bg-green-700 text-white p-8 rounded-2xl text-2xl font-bold flex justify-center items-center h-24"
       >
         🟢 入場口カウンター
-      </a>
-      <a
-        href="/CounterExit"
+      </button>
+
+      <button
+        @click="$emit('selectView', 'exit')"
         class="menu-link bg-red-out hover:bg-red-700 text-white p-8 rounded-2xl text-2xl font-bold flex justify-center items-center h-24"
       >
         🔴 出口カウンター
-      </a>
+      </button>
     </div>
   </div>
 </template>
