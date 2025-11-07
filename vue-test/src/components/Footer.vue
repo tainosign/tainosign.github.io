f9c9937a6f5dd262fcd1be390a9b4bbfa0154145
tainosign <83518282+tainosign@users.noreply.github.com>
2025-11-07 16:43:16 +0900

    Create Footer.vue

---
+++ vue-test/src/components/Footer.vue
@@ -0,0 +1,10 @@
+<template>
+  <footer class="p-4 bg-gray-200 text-gray-700 border-t border-gray-300">
+    <button
+      @click="$emit('back')"
+      class="block w-full text-center bg-gray-400 hover:bg-gray-500 py-3 rounded-lg shadow-md text-white font-semibold text-lg"
+    >
+      メニューへ戻る
+    </button>
+  </footer>
+</template>
