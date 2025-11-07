9173186999c68d48d6c0d2e05d94431bbc6618bb
tainosign <83518282+tainosign@users.noreply.github.com>
2025-11-07 16:54:37 +0900

    Update App.vue

--- vue-test/src/App.vue
+++ vue-test/src/App.vue
@@ -1,13 +1,23 @@
 <template>
-  <div class="flex flex-col min-h-screen bg-gray-100 font-sans">
+  <div>
     <Header />
-    <main class="flex-grow overflow-y-auto">
-      <MainMenu />
+
+    <main>
+      <MainMenu v-if="currentView === 'menu'" @selectView="currentView = $event" />
+      <CounterEntrance v-if="currentView === 'entrance'" @back="currentView = 'menu'" />
+      <CounterLocal v-if="currentView === 'local'" @back="currentView = 'menu'" />
+      <CounterExit v-if="currentView === 'exit'" @back="currentView = 'menu'" />
     </main>
   </div>
 </template>
 
 <script setup>
+import { ref } from "vue";
 import Header from "@/components/Header.vue";
 import MainMenu from "@/components/MainMenu.vue";
+import CounterEntrance from "@/components/CounterEntrance.vue";
+import CounterLocal from "@/components/CounterLocal.vue";
+import CounterExit from "@/components/CounterExit.vue";
+
+const currentView = ref("menu");
 </script>
