40896b06996f0c567db9a596fd106f680e4eaff5
tainosign <83518282+tainosign@users.noreply.github.com>
2025-11-07 09:04:55 +0900

    Update vite.config.ts

--- vue-test/vite.config.ts
+++ vue-test/vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
-  root: 'vue-test',          // ここでビルド対象を vue-test に
   plugins: [vue()],
   build: {
     outDir: '../dist',       // ビルド成果物を project-root/dist に
