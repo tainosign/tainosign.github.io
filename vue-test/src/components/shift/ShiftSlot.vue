ae8f256eeb71ab867874026016d47bc2e2914435
tainosign <83518282+tainosign@users.noreply.github.com>
2025-11-07 23:54:53 +0900

    Create ShiftSlot.vue

---
+++ vue-test/src/components/shift/ShiftSlot.vue
@@ -0,0 +1,15 @@
+<!-- src/components/shift/ShiftSlot.vue -->
+<template>
+  <div class="border text-center rounded"
+       :style="{ minWidth: slotMinWidth + 'px', height: 30 + 'px' }">
+    {{ slot.memberId || '空' }}
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  slot: Object
+})
+
+const slotMinWidth = 20
+</script>
