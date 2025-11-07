fe1931c4b1f67cd2b4c9cff086c06bbd2e92e477
tainosign <83518282+tainosign@users.noreply.github.com>
2025-11-07 14:19:43 +0900

    Update main.ts

--- vue-test/src/main.ts
+++ vue-test/src/main.ts
@@ -1,5 +1,5 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import './index.css'; // TailwindCSSをインポート
+import { createApp } from "vue";
+import App from "./App.vue";
+import "./assets/main.css"; // Tailwind読み込み
 
-createApp(App).mount('#app');
+createApp(App).mount("#app");
