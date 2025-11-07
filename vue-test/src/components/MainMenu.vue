74d8be758e4b6b3dc4299ee29ea7fc0f68ef705c
tainosign <83518282+tainosign@users.noreply.github.com>
2025-11-07 16:50:03 +0900

    Update MainMenu.vue

--- vue-test/src/components/MainMenu.vue
+++ vue-test/src/components/MainMenu.vue
@@ -2,19 +2,19 @@
   <div class="main-content flex flex-col justify-start items-center p-4">
     <div class="w-full max-w-xl flex flex-col gap-4 py-6">
       <a
-        href="/counterLocal"
+        href="/CounterLocal"
         class="menu-link bg-orange-calc hover:bg-orange-500 text-white p-8 rounded-2xl text-2xl font-bold flex justify-center items-center h-24"
       >
         🟠 地域優先入場カウンター
       </a>
       <a
-        href="/counterEntrance"
+        href="/CounterEntrance"
         class="menu-link bg-green-in hover:bg-green-700 text-white p-8 rounded-2xl text-2xl font-bold flex justify-center items-center h-24"
       >
         🟢 入場口カウンター
       </a>
       <a
-        href="/counterExit"
+        href="/CounterExit"
         class="menu-link bg-red-out hover:bg-red-700 text-white p-8 rounded-2xl text-2xl font-bold flex justify-center items-center h-24"
       >
         🔴 出口カウンター
