17808a2dd0cd90df4c32f3976fe7a6065a955dfc
tainosign <83518282+tainosign@users.noreply.github.com>
2025-11-08 00:02:28 +0900

    Update App.vue

--- vue-test/src/App.vue
+++ vue-test/src/App.vue
@@ -1,12 +1,19 @@
 <template>
   <div>
-    <Header />
+    <!-- ヘッダーは currentView が shift の場合は非表示 -->
+    <Header v-if="currentView !== 'shift'" />
 
     <main>
+      <!-- メニュー -->
       <MainMenu v-if="currentView === 'menu'" @selectView="currentView = $event" />
+
+      <!-- カウンター画面 -->
       <CounterEntrance v-if="currentView === 'entrance'" @back="currentView = 'menu'" />
       <CounterLocal v-if="currentView === 'local'" @back="currentView = 'menu'" />
       <CounterExit v-if="currentView === 'exit'" @back="currentView = 'menu'" />
+
+      <!-- シフト管理画面 -->
+      <ShiftCreateView v-if="currentView === 'shift'" @back="currentView = 'menu'" />
     </main>
   </div>
 </template>
@@ -18,6 +25,7 @@ import MainMenu from "@/components/MainMenu.vue";
 import CounterEntrance from "@/components/counters/CounterEntrance.vue";
 import CounterLocal from "@/components/counters/CounterLocal.vue";
 import CounterExit from "@/components/counters/CounterExit.vue";
+import ShiftCreateView from "@/views/ShiftCreateView.vue";
 
 const currentView = ref("menu");
 </script>
