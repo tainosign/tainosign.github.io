aeca4a75c4420e600b67691966c25a5698d1abae
tainosign <83518282+tainosign@users.noreply.github.com>
2025-11-08 22:09:20 +0900

    Update main.ts

--- vue-test/src/main.ts
+++ vue-test/src/main.ts
@@ -1,16 +1,8 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
 import App from "./App.vue";
-import router from "./router"; // ルーター追加
 import "./assets/main.css"; // Tailwind読み込み
 
 const app = createApp(App);
-
-// Pinia登録
-const pinia = createPinia();
-app.use(pinia);
-
-// ルーター登録
-app.use(router);
-
+app.use(createPinia());
 app.mount("#app");
