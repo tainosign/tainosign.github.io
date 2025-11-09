3580591faf617a06a89875e3a7959a47dff4a818
tainosign <83518282+tainosign@users.noreply.github.com>
2025-11-09 11:26:55 +0900

    Update ShiftPosition.vue

--- vue-test/src/components/shift/ShiftPosition.vue
+++ vue-test/src/components/shift/ShiftPosition.vue
@@ -2,35 +2,34 @@
   <ShiftContainer :item="position" :list="positions">
     <template #header>
       <input v-model="position.name" class="border rounded p-1 text-sm w-20" />
-      <button @click="addSlot" class="bg-yellow-500 text-white text-xs px-2 py-1 rounded self-start mt-2">
-          ＋スロット
+      <button @click="addSlot" class="bg-yellow-500 text-white text-xs px-2 py-1 rounded ml-2">
+        ＋スロット
       </button>
     </template>
 
     <template #body>
-      <div class="flex flex-row gap-2">
+      <ScrollableRow>
         <ShiftSlot
           v-for="slot in position.slots"
           :key="slot.id"
           :slot="slot"
           :slots="position.slots"
         />
-      </div>
+      </ScrollableRow>
     </template>
   </ShiftContainer>
 </template>
 
 <script setup>
 import ShiftContainer from "./ShiftContainer.vue";
+import ScrollableRow from "../common/ScrollableRow.vue";
 import ShiftSlot from "./ShiftSlot.vue";
 import { createSlot } from "@/models/shiftModel";
 
 const props = defineProps({
   position: Object,
-  positions: Array, // 親リスト
+  positions: Array,
 });
 
-const addSlot = () => {
-  props.position.slots.push(createSlot());
-};
+const addSlot = () => props.position.slots.push(createSlot());
 </script>
