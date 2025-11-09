a8d36572eecab7fe9170e0c317334d259c565f61
tainosign <83518282+tainosign@users.noreply.github.com>
2025-11-09 11:26:11 +0900

    Update ShiftTeam.vue

--- vue-test/src/components/shift/ShiftTeam.vue
+++ vue-test/src/components/shift/ShiftTeam.vue
@@ -2,26 +2,27 @@
   <ShiftContainer :item="team" :list="teams">
     <template #header>
       <input v-model="team.name" class="border rounded p-1 text-sm w-24" />
-      <button @click="addPosition" class="bg-blue-500 text-white text-xs px-2 py-1 rounded self-start mt-2">
-          ＋ポジション
+      <button @click="addPosition" class="bg-blue-500 text-white text-xs px-2 py-1 rounded ml-2">
+        ＋ポジション
       </button>
     </template>
 
     <template #body>
-      <div class="flex flex-row gap-2 items-start">
+      <ScrollableRow>
         <ShiftPosition
           v-for="position in team.positions"
           :key="position.id"
           :position="position"
           :positions="team.positions"
         />
-      </div>
+      </ScrollableRow>
     </template>
   </ShiftContainer>
 </template>
 
 <script setup>
 import ShiftContainer from "./ShiftContainer.vue";
+import ScrollableRow from "../common/ScrollableRow.vue";
 import ShiftPosition from "./ShiftPosition.vue";
 import { createPosition } from "@/models/shiftModel";
 
@@ -30,7 +31,5 @@ const props = defineProps({
   teams: Array,
 });
 
-const addPosition = () => {
-  props.team.positions.push(createPosition());
-};
+const addPosition = () => props.team.positions.push(createPosition());
 </script>
