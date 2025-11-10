caa6b408e4c27d4ec3575dcdf978b82e0869a096
tainosign <83518282+tainosign@users.noreply.github.com>
2025-11-10 15:17:15 +0900

    Create Home.vue

---
+++ vue-test/src/views/Home.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="p-4">
+    <h1 class="text-xl font-bold">ホーム画面</h1>
+    <p>ここがメニューやリンクの表示場所です。</p>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+</style>
