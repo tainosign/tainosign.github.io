db52478b211cb35936cdabcb0f6b063722c28332
tainosign <83518282+tainosign@users.noreply.github.com>
2025-11-10 16:54:28 +0900

    Update useFirestoreMembers.js

--- vue-test/src/composables/useFirestoreMembers.js
+++ vue-test/src/composables/useFirestoreMembers.js
@@ -1,50 +1,59 @@
 // src/composables/useFirestoreMembers.js
 import { useFirebase } from "./useFirebase.js";
-import { collection, doc, setDoc, getDocs, updateDoc, deleteDoc, onSnapshot } from "firebase/firestore";
-import { v4 as uuidv4 } from "uuid";
-import { createMemberModel } from "../models/shiftModel.js";
+import {
+  collection,
+  doc,
+  setDoc,
+  getDoc,
+  updateDoc,
+  deleteDoc,
+  getDocs,
+  onSnapshot,
+} from "firebase/firestore";
+import { createMemberModel } from "../models/memberModel.js";
 
 export function useFirestoreMembers() {
-  const init = async () => {
+  const initColRef = async () => {
     const { db } = await useFirebase();
-    const colRef = collection(db, "members");
-    return { db, colRef };
+    return collection(db, "members");
   };
 
   const addMember = async (data) => {
-    const { colRef } = await init();
-    const member = createMemberModel({ ...data, token: uuidv4() });
+    const colRef = await initColRef();
+    const member = createMemberModel(data);
     await setDoc(doc(colRef, member.id), member);
     return member;
   };
 
   const getMembers = async () => {
-    const { colRef } = await init();
+    const colRef = await initColRef();
     const snap = await getDocs(colRef);
-    return snap.docs.map(d => d.data());
+    return snap.docs.map((d) => d.data());
   };
 
   const updateMember = async (id, updates) => {
-    const { colRef } = await init();
+    const colRef = await initColRef();
     await updateDoc(doc(colRef, id), { ...updates, updated_at: new Date() });
   };
 
   const deleteMember = async (id) => {
-    const { colRef } = await init();
+    const colRef = await initColRef();
     await deleteDoc(doc(colRef, id));
   };
 
-  const getMemberByToken = async (token) => {
-    const all = await getMembers();
-    return all.find(m => m.token === token) || null;
-  };
-
   const syncMembers = async (callback) => {
-    const { colRef } = await init();
+    const colRef = await initColRef();
     return onSnapshot(colRef, (snapshot) => {
-      callback(snapshot.docs.map(d => d.data()));
+      const data = snapshot.docs.map((d) => d.data());
+      callback(data);
     });
   };
 
-  return { addMember, getMembers, updateMember, deleteMember, getMemberByToken, syncMembers };
+  return {
+    addMember,
+    getMembers,
+    updateMember,
+    deleteMember,
+    syncMembers,
+  };
 }
