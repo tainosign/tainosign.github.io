bd867dcf4992c93c545a9862240482893ca81515
tainosign <83518282+tainosign@users.noreply.github.com>
2025-11-10 14:45:13 +0900

    Update App.vue

--- vue-test/src/App.vue
+++ vue-test/src/App.vue
@@ -14,6 +14,9 @@
 
       <!-- シフト管理画面 -->
       <ShiftCreateView v-if="currentView === 'shift'" @back="currentView = 'menu'" />
+      <router-link to="/test-members" class="px-2 py-1 bg-gray-200 rounded">
+  テスト登録
+</router-link>
     </main>
   </div>
 </template>
