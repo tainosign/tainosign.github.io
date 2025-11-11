f6b87090919d38ff433a3d9805142bedd6d479cb
tainosign <83518282+tainosign@users.noreply.github.com>
2025-11-11 20:43:46 +0900

    Update index.ts

--- vue-test/src/router/index.ts
+++ vue-test/src/router/index.ts
@@ -1,14 +1,14 @@
 import { createRouter, createWebHistory } from "vue-router";
-import App from "@/App.vue"; // トップページ
+import TopMenu from "@/views/TopMenu.vue";
 import OpenSandSurvey from "@/views/OpenSandSurvey.vue";
 import MemberView from "@/views/MemberView.vue";
 import LeaderMenu from "@/views/LeaderMenu.vue";
 
 const routes = [
-  { path: "/", name: "AppTop", component: App }, // 既存トップメニュー
+  { path: "/", name: "TopMenu", component: TopMenu }, // トップメニュー
   { path: "/OPENSAND", name: "OpenSandSurvey", component: OpenSandSurvey },
   { path: "/:memberId", name: "MemberView", component: MemberView },
-  { path: "/jg6p2jg6z", name: "LeaderMenu", component: LeaderMenu }, // リーダーページ
+  { path: "/jg6p2jg6z", name: "LeaderMenu", component: LeaderMenu },
 ];
 
 export default createRouter({
