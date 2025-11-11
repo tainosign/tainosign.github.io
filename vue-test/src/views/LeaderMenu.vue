65a50bb099e7d6ac8fabbe26f5ba89ac2a94122b
tainosign <83518282+tainosign@users.noreply.github.com>
2025-11-11 20:15:31 +0900

    Create LeaderMenu.vue

---
+++ vue-test/src/views/LeaderMenu.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <Header v-if="currentView !== 'shift'" />
+
+    <main class="p-4">
+      <!-- メニュー -->
+      <MainMenu v-if="currentView === 'menu'" @selectView="currentView = $event" />
+      <!-- カウンター画面 -->
+      <CounterEntrance v-if="currentView === 'entrance'" @back="currentView = 'menu'" />
+      <CounterLocal v-if="currentView === 'local'" @back="currentView = 'menu'" />
+      <CounterExit v-if="currentView === 'exit'" @back="currentView = 'menu'" />
+
+      <!-- シフト管理画面 -->
+      <ShiftCreateView v-if="currentView === 'shift'" @back="currentView = 'menu'" />
+    </main>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import Header from "@/components/Header.vue";
+import MainMenu from "@/components/MainMenu.vue";
+import CounterEntrance from "@/components/counters/CounterEntrance.vue";
+import CounterLocal from "@/components/counters/CounterLocal.vue";
+import CounterExit from "@/components/counters/CounterExit.vue";
+import ShiftCreateView from "@/views/ShiftCreateView.vue";
+
+const currentView = ref("menu");
+</script>
