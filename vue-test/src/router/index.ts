fdea96f94d55c2248043b7b7038d9f22117c68b0
tainosign <83518282+tainosign@users.noreply.github.com>
2025-11-11 19:18:27 +0900

    Update index.ts

--- vue-test/src/router/index.ts
+++ vue-test/src/router/index.ts
@@ -2,12 +2,12 @@ import { createRouter, createWebHistory } from "vue-router";
 //import LeaderMenu from "@/views/LeaderMenu.vue";
 import App from "@/App.vue";
 import OpenSandSurvey from "@/views/OpenSandSurvey.vue";
-import MemberPage from "@/views/MemberPage.vue";
+import MemberView from "@/views/MemberView.vue";
 
 const routes = [
   { path: "/jg6p2jg6z", name: "LeaderMenu", component: App },
   { path: "/OPENSAND", name: "OpenSandSurvey", component: OpenSandSurvey },
-  { path: "/:memberId", name: "MemberPage", component: MemberPage },
+  { path: "/:memberId", name: "MemberView", component: MemberView },
 ];
 
 export default createRouter({
