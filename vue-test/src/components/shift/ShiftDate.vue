dbdcd3932bce5d35e240bec3e868443797c82175
tainosign <83518282+tainosign@users.noreply.github.com>
2025-11-12 21:22:25 +0900

    Update ShiftDate.vue

--- vue-test/src/components/shift/ShiftDate.vue
+++ vue-test/src/components/shift/ShiftDate.vue
@@ -1,27 +1,5 @@
 <template>
   <div>
-    <!-- 日付選択複数 -->
-    <div class="flex gap-2 mb-2">
-      <input
-        type="date"
-        multiple
-        v-model="selectedDates"
-        class="border rounded p-1 text-sm"
-      />
-      <button
-        @click="loadSelectedDates"
-        class="bg-blue-500 text-white text-xs px-2 py-1 rounded"
-      >
-        読み込み
-      </button>
-      <button
-        @click="saveSelectedDates"
-        class="bg-green-500 text-white text-xs px-2 py-1 rounded"
-      >
-        保存
-      </button>
-    </div>
-
     <!-- 各日付のシフト -->
     <div v-for="shift in shiftStore.shifts" :key="shift.date" class="mb-4">
       <ShiftContainer :item="shift" :list="shiftStore.shifts">
@@ -53,27 +31,11 @@
 </template>
 
 <script setup>
-import { ref } from "vue";
 import { useShiftStore } from "@/stores/shiftStore";
 import ShiftContainer from "./ShiftContainer.vue";
 import ShiftTeam from "./ShiftTeam.vue";
 import ScrollableRow from "../common/ScrollableRow.vue";
 import { createTeam } from "@/models/shiftModel";
-
-const shiftStore = useShiftStore();
-const selectedDates = ref([]);
-
-const loadSelectedDates = async () => {
-  for (const d of selectedDates.value) {
-    await shiftStore.loadShiftByDate(d);
-  }
-};
-
-const saveSelectedDates = async () => {
-  for (const d of selectedDates.value) {
-    await shiftStore.saveShift(d);
-  }
-};
 
 const addTeam = (shift) => {
   shift.teams.push(createTeam());
