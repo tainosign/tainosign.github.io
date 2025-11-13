3cd1ce3b5f6e151b9fd812b0b96535442b0969f5
tainosign <83518282+tainosign@users.noreply.github.com>
2025-11-13 20:02:45 +0900

    Update ShiftDate.vue

--- vue-test/src/components/shift/ShiftDate.vue
+++ vue-test/src/components/shift/ShiftDate.vue
@@ -1,40 +1,42 @@
 <template>
   <div class="p-2 space-y-2">
-    <div v-for="shift in shiftStore.shifts" :key="shift.date">
-      <ShiftContainer :item="shift" :list="shiftStore.shifts" type="shift">
-        <template #header>
-          <div class="flex items-center gap-2">
-            <span class="font-bold">{{ shift.date }}</span>
-            <button
-              @click="addTeam(shift.date)"
-              class="bg-green-500 text-white text-xs px-2 py-1 rounded"
-            >
-              ＋チーム
-            </button>
-          </div>
-        </template>
+    <ShiftContainer :item="shift" :list="[shift]" type="shift">
+      <template #header>
+        <div class="flex items-center gap-2">
+          <span class="font-bold">{{ shift.date }}</span>
+          <button
+            @click="addTeam(shift.date)"
+            class="bg-green-500 text-white text-xs px-2 py-1 rounded"
+          >
+            ＋チーム
+          </button>
+        </div>
+      </template>
 
-        <template #body>
-          <ScrollableRow>
-            <ShiftTeam
-              v-for="team in shift.teams"
-              :key="team.id"
-              :shift-date="shift.date"
-              :team="team"
-            />
-          </ScrollableRow>
-        </template>
-      </ShiftContainer>
-    </div>
+      <template #body>
+        <ScrollableRow>
+          <ShiftTeam
+            v-for="team in shift.teams"
+            :key="team.id"
+            :shift-date="shift.date"
+            :team="team"
+          />
+        </ScrollableRow>
+      </template>
+    </ShiftContainer>
   </div>
 </template>
 
 <script setup>
-import { useShiftStore } from "@/stores/shiftStore";
 import ShiftContainer from "./ShiftContainer.vue";
 import ShiftTeam from "./ShiftTeam.vue";
 import ScrollableRow from "../common/ScrollableRow.vue";
 
+const props = defineProps({
+  shift: Object
+});
+
+import { useShiftStore } from "@/stores/shiftStore";
 const shiftStore = useShiftStore();
 
 const addTeam = (date) => {
